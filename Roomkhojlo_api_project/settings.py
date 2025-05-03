3b77098271d5a66dcbc0b4c694eef5ab23b42e59
RohitRaushan <rohitraushan12@gmail.com>
2025-05-03 23:29:02 +0530

    connection mysql workbech to railway

--- Roomkhojlo_api_project/settings.py
+++ Roomkhojlo_api_project/settings.py
@@ -97,15 +97,8 @@ WSGI_APPLICATION = 'Roomkhojlo_api_project.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
-
-# Use MySQL on Railway (if DATABASE_URL is set)
-if os.environ.get("mysql://root:XSMTPiHfUudRkBhCEkEFhfhQlpBeeweV@mysql.railway.internal:3306/railway"):
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
